81a45a7b1f4b9a2f513f044d3a796679aba48eb7
Dima Danilchenko <switch57dima@yandex.ru>
2023-02-10 12:08:47 +0300

    5 проект

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
 // Находим форму в DOM
 const profileForm = document.querySelector('.profile-popup__form'),
-      formAddPhoto = document.querySelector('.add-photo-popup__form'),
-      profileTitle = document.querySelector('.profile__title'),
-      profileSubtitle = document.querySelector('.profile__subtitle'),
-      profilePopup = document.querySelector('.profile-popup'),
-      popupAdd = document.querySelector('.add-photo-popup'),
-      profileRedaction = document.querySelector('.profile__redaction'),
-      popupCloseAdd = document.querySelector('.add-photo-popup__close'),
-// Находим поля формы в DOM
-      nameInput = document.querySelector('.profile-popup__input_type_name'),
-      jobInput = document.querySelector('.profile-popup__input_type_job'),
-      namePhotoInput = document.querySelector('.add-photo-popup__input_type_photo'),
-      linkInput = document.querySelector('.add-photo-popup__input_type_link'),
-      photoElements = document.querySelector('.elements'),
-      addPhoto = document.querySelector('.profile__add'),
-      cardTemplate = document.querySelector('#element-template').content,
-      popupPhoto = document.querySelector('.photo-popup'),
-      photoForPopup = popupPhoto.querySelector('.photo-popup__photo'),
-      textForPopup = popupPhoto.querySelector('.photo-popup__text');
+  formAddPhoto = document.querySelector('.add-photo-popup__form'),
+  profileTitle = document.querySelector('.profile__title'),
+  profileSubtitle = document.querySelector('.profile__subtitle'),
+  profilePopup = document.querySelector('.profile-popup'),
+  popupAdd = document.querySelector('.add-photo-popup'),
+  profileRedaction = document.querySelector('.profile__redaction'),
+  popupCloseAdd = document.querySelector('.add-photo-popup__close'),
+  // Находим поля формы в DOM
+  nameInput = document.querySelector('.profile-popup__input_type_name'),
+  jobInput = document.querySelector('.profile-popup__input_type_job'),
+  namePhotoInput = document.querySelector('.add-photo-popup__input_type_photo'),
+  linkInput = document.querySelector('.add-photo-popup__input_type_link'),
+  photoElements = document.querySelector('.elements'),
+  addPhoto = document.querySelector('.profile__add'),
+  cardTemplate = document.querySelector('#element-template').content,
+  popupPhoto = document.querySelector('.photo-popup'),
+  photoForPopup = popupPhoto.querySelector('.photo-popup__photo'),
+  textForPopup = popupPhoto.querySelector('.photo-popup__text');
 
 const initialCards = [
   {
@@ -52,7 +52,7 @@ initialCards.forEach((item) => {
   photoElements.append(createCard(item));
 });
 
-function openPopup(element){
+function openPopup(element) {
   element.classList.add('popup_open');
 };
 // находим все крестики проекта по универсальному селектору
@@ -69,22 +69,22 @@ function closePopup(element) {
   element.classList.remove('popup_open');
 };
 
-function handleFormSubmit (evt, popupElement) {
+function handleFormSubmit(evt, popupElement) {
   evt.preventDefault();
   closePopup(popupElement);
 };
-function changeProfile(){
-  profileTitle.innerText =  nameInput.value;
+function changeProfile() {
+  profileTitle.innerText = nameInput.value;
   profileSubtitle.innerText = jobInput.value;
 };
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-profileForm.addEventListener('submit', (evt)=>{
+profileForm.addEventListener('submit', (evt) => {
   handleFormSubmit(evt, profilePopup);
   changeProfile();
 });
-formAddPhoto.addEventListener('submit', (evt)=>{
+formAddPhoto.addEventListener('submit', (evt) => {
   handleFormSubmit(evt, popupAdd);
   const addObjPhoto = {};
   console.log(namePhotoInput);
@@ -94,7 +94,7 @@ formAddPhoto.addEventListener('submit', (evt)=>{
   evt.target.reset();
 });
 
-profileRedaction.addEventListener('click', ()=>{
+profileRedaction.addEventListener('click', () => {
   openPopup(profilePopup);
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
@@ -108,7 +108,7 @@ addPhoto.addEventListener('click', (evt) => {
 
 function createCard(item) {
   const userPhoto = cardTemplate.querySelector('.element').cloneNode(true),
-  elementImage = userPhoto.querySelector('.element__image');
+    elementImage = userPhoto.querySelector('.element__image');
 
   elementImage.src = item.link;
   userPhoto.querySelector('.element__text').textContent = item.name;
@@ -120,9 +120,9 @@ function createCard(item) {
 
   elementImage.addEventListener("click", () => {
     openPopup(popupPhoto);
-      photoForPopup.src = item.link;
-      textForPopup.textContent = item.name;
-      photoForPopup.alt = item.name;
+    photoForPopup.src = item.link;
+    textForPopup.textContent = item.name;
+    photoForPopup.alt = item.name;
   });
 
   userPhoto.querySelector('.element__delete').addEventListener("click", () => {
