cc2b2a1d8f71e1cd4ce5d8d92c2d5afb30585296
Dima Danilchenko <switch57dima@yandex.ru>
2023-03-18 19:13:55 +0300

    123

---
+++ scripts/Card.js
@@ -0,0 +1,80 @@
+const photoElements = document.querySelector('.elements');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+class Card {
+  constructor(data, selector) {
+    this._name = data.name;
+    this._link = data.link;
+    this._selector = selector;
+  };
+  _getElement() {
+    const cardElement = document.querySelector(this._selector).content.querySelector('.element').cloneNode(true);
+    return cardElement;
+	};
+  generate() {
+    this._element = this._getElement();
+  	this._element.querySelector('.element__image').src = this._link;
+  	this._element.querySelector('.element__text').textContent = this._name;
+
+    this._setEventListeners();
+
+  	return this._element;
+  };
+
+  _setEventListeners() {
+    this._element.querySelector('.element__heart').addEventListener('click', () => {
+      this._handleClick();
+    });
+
+    this._element.querySelector('.element__image').addEventListener('click', () => {
+      openPopup(popupPhoto);
+      photoForPopup.src = this.link;
+      textForPopup.textContent = this.name;
+      photoForPopup.alt = this.name;
+    });
+
+    this._element.querySelector('.element__delete').addEventListener("click", () => {
+      this._element.remove();
+    });
+  };
+
+  _handleClick() {
+    this._element.querySelector('.element__heart').classList.toggle("element__heart_active");
+  };
+};
+function createCard(item){
+  const card = new Card(item, '.element-template_type_default');
+  const cardElement = card.generate();
+  photoElements.append(cardElement);
+}
+// Просмотр массива
+initialCards.forEach((item) => {
+  createCard(item);
+});
+export { Card, initialCards, createCard};
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-
+import {Card, initialCards, createCard} from './Card.js';
+import {objData, Validator} from './FormValidator.js';
 // Находим форму в DOM
 const profileForm = document.querySelector('.profile-popup__form'),
   formAddPhoto = document.querySelector('.add-photo-popup__form'),
@@ -13,46 +14,13 @@ const profileForm = document.querySelector('.profile-popup__form'),
   jobInput = document.querySelector('.profile-popup__input_type_job'),
   namePhotoInput = document.querySelector('.add-photo-popup__input_type_photo'),
   linkInput = document.querySelector('.add-photo-popup__input_type_link'),
-  photoElements = document.querySelector('.elements'),
   addPhoto = document.querySelector('.profile__add'),
-  cardTemplate = document.querySelector('#element-template').content,
   popupPhoto = document.querySelector('.photo-popup'),
   photoForPopup = popupPhoto.querySelector('.photo-popup__photo'),
   textForPopup = popupPhoto.querySelector('.photo-popup__text');
 const allForm = Array.from(document.querySelectorAll('.popup'));
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
-
-// Просмотр массива
-initialCards.forEach((item) => {
-  photoElements.append(createCard(item));
-});
 
 function openPopup(element) {
   element.classList.add('popup_open');
@@ -94,7 +62,7 @@ formAddPhoto.addEventListener('submit', (evt) => {
   console.log(namePhotoInput);
   addObjPhoto.name = namePhotoInput.value;
   addObjPhoto.link = linkInput.value;
-  photoElements.prepend(createCard(addObjPhoto));
+  createCard(addObjPhoto);
   formAddPhoto.querySelector('.add-photo-popup__save ').disabled = 'true';
   formAddPhoto.querySelector('.add-photo-popup__save ').classList.add('popup__button_disabled');
   evt.target.reset();
@@ -110,32 +78,6 @@ profileRedaction.addEventListener('click', () => {
 addPhoto.addEventListener('click', (evt) => {
   openPopup(popupAdd);
 })
-
-
-function createCard(item) {
-  const userPhoto = cardTemplate.querySelector('.element').cloneNode(true),
-    elementImage = userPhoto.querySelector('.element__image');
-
-  elementImage.src = item.link;
-  userPhoto.querySelector('.element__text').textContent = item.name;
-  elementImage.alt = item.name;
-
-  userPhoto.querySelector('.element__heart').addEventListener("click", () => {
-    userPhoto.querySelector('.element__heart').classList.toggle("element__heart_active");
-  });
-
-  elementImage.addEventListener("click", () => {
-    openPopup(popupPhoto);
-    photoForPopup.src = item.link;
-    textForPopup.textContent = item.name;
-    photoForPopup.alt = item.name;
-  });
-
-  userPhoto.querySelector('.element__delete').addEventListener("click", () => {
-    userPhoto.remove();
-  });
-  return userPhoto;
-}
 
 // Close popup all------------------
 allForm.forEach((elem) => {
